677d7a4502fd2ed3e40d0edf488032ae44b54e86
Michael Dettbarn <michael.dettbarn@physik.uni-wuerzburg.de>
2020-07-20 09:57:50 +0200

    Add a wrapper script.

---
+++ make-all.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -f *.ipf ]; then
+	echo "Error in $(basename $0): No procedure file found."
+	exit 1
+fi
+
+./make-dokuwiki.sh *.ipf
+
+echo "All documentations have been generated."
