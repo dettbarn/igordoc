910947fbae7fdeec001cfc515fde9df0a940e3bb
Michael Dettbarn <michael.dettbarn@physik.uni-wuerzburg.de>
2020-10-03 19:24:48 +0200

    Fix redundant and unclear function definitions.

--- make-dokuwiki.sh
+++ make-dokuwiki.sh
@@ -8,34 +8,16 @@ if [ -f $outf ]; then
 	mv $outf $outf_old
 fi
 
-# search for function headers, perhaps predeced by commented lines
-function fn_grep {
-	pcregrep -M '(//.*\r?\n)*Function' $1
-}
-
-# move function header before the comments
-function fn_reorder {
-	perl -0777 -i.original -pe 's/(\/\/(===|---).*\r?\n?)((\/\/.*\r?\n?)*)(Function.*\r?\n?)/\n\5\3\n/g' $1
-}
+source general.sh
 
 # dokuwiki function formatting
 function fn_format_dokuwiki {
 	sed "s/\(\(Function.*\) \([^( ]\+\) \?([^)]*).*$\)/==== \3 ====\n''\1''/g" $1
 }
 
-# remove double-slashes before comments
-function comment_format {
-	sed 's/\/\/\(.*\)/\1/g' $1
-}
-
 # add linebreaks, but not after headings
-function add_breaks {
+function add_breaks_dokuwiki {
 	sed 's/\(.*[^=-]$\)/\1\\\\/g' $1
-}
-
-# remove single spaces at beginning of lines
-function rm_front_spaces {
-	sed 's/^ \(.*\)/\1/g' $1
 }
 
 # pipe documentation of all procedures into dokuwiki formatted file
@@ -47,7 +29,7 @@ do
 		| fn_reorder \
 		| fn_format_dokuwiki \
 		| comment_format \
-		| add_breaks \
+		| add_breaks_dokuwiki \
 		| rm_front_spaces \
 		>> $outf
 done
--- make-tex.sh
+++ make-tex.sh
@@ -8,39 +8,16 @@ if [ -f $outf ]; then
 	mv $outf $outf_old
 fi
 
-# search for function headers, perhaps predeced by commented lines
-function fn_grep {
-	pcregrep -M '(//.*\r?\n)*Function' $1
-}
-
-# move function header before the comments
-function fn_reorder {
-	perl -0777 -i.original -pe 's/(\/\/(===|---).*\r?\n?)((\/\/.*\r?\n?)*)(Function.*\r?\n?)/\n\5\3\n/g' $1
-}
+source general.sh
 
 # tex function formatting
 function fn_format_tex {
 	sed "s/\(\(Function.*\) \([^( ]\+\) \?([^)]*)[^\\r]*\)/\\\subsection*{\3}\n\\\\texttt{\1}\n/g" $1
 }
 
-# remove double-slashes before comments
-function comment_format {
-	sed 's/\/\/\(.*\)/\1/g' $1
-}
-
-# remove carriage returns
-function rm_cr {
-        sed 's/\r//g'
-}
-
 # add linebreaks, but not after headings
 function add_breaks_tex {
 	sed '/\\subsection\|^$/!s/\([^\r]*\)/\1~\\\\\n/g' $1
-}
-
-# remove single spaces at beginning of lines
-function rm_front_spaces {
-	sed 's/^ \(.*\)/\1/g' $1
 }
 
 # escape some special characters
